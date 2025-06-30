69fb5c54659db240ac832fec4998a6a2d30bc3b1
Виталий Гульчеев <vitaliygulcheev@MacBook-Pro-Vitalij.local>
2025-07-01 00:11:14 +0300

    удалил старое

--- client/src/components/courses/lesson-templates/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as HookTemplate } from './hook-template';
-export { default as ExplainDemoTemplate } from './explain-demo-template';
-export { default as QuickTryReflectTemplate } from './quick-try-reflect-template';
-export { default as InteractiveQuickTryTemplate } from './interactive-quick-try-template';
-export { default as SimplifiedExplainDemo } from './simplified-explain-demo';
-export { default as SimplifiedQuickTryReflect } from './simplified-quick-try-reflect';
-export type { InteractiveElement, InteractiveElementType } from './interactive-quick-try-template';
