f05fab5ea8d4ba6a70e222a3bfb7d1d74d807759
Виталий Гульчеев <vitaliygulcheev@MacBook-Pro-Vitalij.local>
2025-07-02 02:02:03 +0300

    удалил

--- client/src/components/courses/interactive/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MultipleChoiceQuiz } from './multiple-choice-quiz';
-export { default as PracticalAssignment } from './practical-assignment';
-export type { QuizOption } from './multiple-choice-quiz';
-export type { PracticalAssignmentProps } from './practical-assignment';
