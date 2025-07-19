579ed909ece0d0554eef99373c3e4b399ac4ad46
BAWION <132302507+BAWION@users.noreply.github.com>
2025-07-19 17:02:25 +0300

    Update index.ts

--- client/src/data/index.ts
+++ client/src/data/index.ts
@@ -1,2 +1,5 @@
+export { aiEthicsToolkit } from './ai-ethics-toolkit';
+export { aiEthicsToolkitV2 } from './ai-ethics-toolkit-v2';
+export { coursesCatalog } from './courses-catalog';
 export * from './ethics-course';
 export * from './law-course';
